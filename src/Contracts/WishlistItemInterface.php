1ed8fc15381587dfce0e04fe617d3d6f447c1f4e
Nathan Geerinck <nathan@tnt.be>
2021-09-16 16:34:35 +0200

    Formatting

--- src/Contracts/WishlistItemInterface.php
+++ src/Contracts/WishlistItemInterface.php
@@ -4,11 +4,11 @@ namespace Tnt\Wishlist\Contracts;
 
 interface WishlistItemInterface
 {
-	public static function getByWishlistId(int $id): ?WishlistItemInterface;
+    public static function getByWishlistId(int $id): ?WishlistItemInterface;
 
-	public function getWishlistId(): int;
+    public function getWishlistId(): int;
 
-	public function serialize(): array;
+    public function serialize(): array;
 
     public function isWishlistable(): bool;
 }
